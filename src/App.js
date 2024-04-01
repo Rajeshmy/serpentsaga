8a7d812fd98ff0200fee0802b040d3485ecddac7
rajesh.my1-v <rajesh.my1-v@abfrl.adityabirla.com>
2024-04-01 11:58:20 +0530

    style changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Button, ChakraProvider, Container, Heading } from "@chakra-ui/react";
+import { Box, Button, ChakraProvider, Container, Heading } from "@chakra-ui/react";
 import CanvasBoard from "../src/components/CanvasBoard";
 import { useSelector } from "react-redux";
 import { setdifficulty } from "./utilities/difficulty";
@@ -13,26 +13,32 @@ function App() {
   }
 
   return (
-    <ChakraProvider >
-      <Heading size="l" color={'red'} marginRight={"2%"} marginLeft={"2%"}  marginTop={"8%"}>
+    <ChakraProvider>
+      <Box bg="teal.100"
+        width="100%"
+        height="100%"
+        py="4">
+      
+      <Container maxW="container.lg" centerContent  backgroundColor={"#bddad0"} display="flex" justifyContent="center" alignSelf="center" borderRadius={"20px"} >
+      <div style={{display:"flex",flexDirection:'row', justifyContent:'space-evenly',backgroundColor:'rgba(0,0,0,0.1)', width:"100%",alignItems:'center',height:"120px",borderTopLeftRadius:'20px',borderTopRightRadius:'20px'}}>
+      <Heading size="l" color={'red'} >
          Difficulty Level
-         <br></br>
          <select onChange={handlelevelchange}>
           <option value="180">Easy</option>
           <option value="140">Medium</option>
           <option value="90">Hard</option>
          </select>
-         
       </Heading>
-      <Container maxW="container.lg" centerContent  backgroundColor={"#bddad0"} display="flex" justifyContent="center" alignSelf="center" borderBottomRadius={"20px"}>
         <Heading as="h1" size="xl">
           SNAKE GAME 
         </Heading>
         <Heading size="l" color={'red'} marginBottom={"10px"}>
          SCORE : {score}
         </Heading>
+        </div>
         <CanvasBoard height={600} width={1000} /> 
       </Container>
+      </Box>
     </ChakraProvider>
   );
 }
